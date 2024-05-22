8012b6b9caabfc3fbf12cc805384f06965fa466e
Jackson Werner <74575896+cheeem@users.noreply.github.com>
2024-05-21 22:53:56 -0400

    Post Upload Api Route

--- server/src/main.rs
+++ server/src/main.rs
@@ -13,11 +13,16 @@ use sqlx::mysql::MySqlPoolOptions;
 // standard library
 use std::env;
 use std::sync::Arc;
+use std::sync::Mutex;
+use std::fs;
+use std::ffi;
 // internal modules
 mod routes;
 
 pub struct AppState {
     db: Pool<MySql>,
+    img_path: String, 
+    img_count: Mutex<u32>,
 }
 
 #[tokio::main]
@@ -29,6 +34,10 @@ async fn main() {
 
     let server_addr: &str = &env::var("SERVER_ADDR").expect("server addr env");
     let client_addr: &str = &env::var("CLIENT_ADDR").expect("client addr env");
+    
+    let img_path: String = env::var("IMG_PATH").expect("img path env");
+
+    let img_count: Mutex<u32> = Mutex::new(get_img_count(&img_path));
 
     let cors: CorsLayer = CorsLayer::new()
         .allow_origin(client_addr.parse::<HeaderValue>().expect("client addr parse"))
@@ -43,11 +52,14 @@ async fn main() {
 
     let state: Arc<AppState> = Arc::new(AppState {
         db,
+        img_path,
+        img_count,
     });
 
     let app: Router = Router::new() 
         .route("/home/get_classes_by_code", get(routes::home::get_classes_by_code))
         .route("/home/get_classes_by_professor", get(routes::home::get_classes_by_professor))
+        .route("/upload/upload", post(routes::upload::upload))
         .layer(cors)
         .with_state(state.clone());
 
@@ -60,5 +72,31 @@ async fn main() {
     axum::serve(listener, app)
         .await
         .expect("axum serve");
+
+}
+
+fn get_img_count(img_path: &str) -> u32 {
+
+    let dir: fs::ReadDir = fs::read_dir(img_path).expect("read img dir");
+
+    let mut img_count: u32 = 0;
+
+    for entry in dir {
+        
+        let file_name: ffi::OsString = entry.expect("img entry error").file_name();
+        let file_name: &str = file_name.to_str().expect("img file name not found");
+
+        let dot_pos: usize = file_name.rfind('.').expect("img file has not file extension");
+        let file_name: &str = &file_name[..dot_pos];
+        
+        let img_idx: u32 = file_name.parse().expect("img file name is not numerical");
+        
+        if img_idx > img_count {
+            img_count = img_idx
+        }
+
+    }
+
+    img_count
 
 }
