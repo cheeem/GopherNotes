5531975e65cb0dcf38037add4c8e395d40e640c5
Jackson Werner <74575896+cheeem@users.noreply.github.com>
2025-01-19 22:37:17 -0600

    Upload Error Messages

--- client/src/routes/Upload/Upload.tsx
+++ client/src/routes/Upload/Upload.tsx
@@ -4,43 +4,56 @@ import { base_api_url } from "../../constants";
 import svg_upload from "../../img/Upload.svg";
 import "./Upload.css";
 
+type UploadError = {
+    status: number,
+    field?: string,
+    msg: string,
+};
+
 export default function Upload() {
 
     const navigate: NavigateFunction = useNavigate();
 
     const [upload_type_is_file, setUploadTypeIsFile] = useState(true);
+    const [error, setError] = useState<UploadError | null>(null);
 
     return (
         <article id="upload">
-            <form action={`${base_api_url}/upload/upload`} onSubmit={(e: FormEvent<HTMLFormElement>) => upload(e, navigate)}>
+            <form action={`${base_api_url}/upload/upload`} onSubmit={(e: FormEvent<HTMLFormElement>) => upload(e, navigate, setError)}>
                 <div className="form-header">
                     <h1>Post to Gopher<span>Notes</span></h1>
                 </div>
                 <ul>
                     <li>
+                        {error && error.field && "department_code".includes(error.field) ? <ErrorMessage error={error} /> : null}
                         <label htmlFor="upload-department-code">Department Code</label>
                         <input id="upload-department-code" type="text" name="department_code"  required />
                     </li>
                     <li>
+                        {error && error.field && "class_code".includes(error.field) ? <ErrorMessage error={error} /> : null}
                         <label htmlFor="upload-class-code">Class Code</label>
                         <input id="upload-class-code" type="text" name="class_code" />
                     </li>
                     <li>
+                        {error && error.field && error.field === "title" ? <ErrorMessage error={error} /> : null}
                         <label htmlFor="upload-title">Post Title</label>
                         <input id="upload-title" type="text" name="title" />
                     </li>
                     <li className="upload-field">
+                        {error && error.field && error.field === "file" && upload_type_is_file ? <ErrorMessage error={error} /> : null}
+                        {error && error.field && error.field === "text" && upload_type_is_file === false ? <ErrorMessage error={error} /> : null}
                         <div className="upload-type">
                             <button type="button" className={upload_type_is_file ? "active" : ""} onClick={() => setUploadTypeIsFile(true)}>File</button>
                             <button type="button" className={upload_type_is_file ? "" : "active"} onClick={() => setUploadTypeIsFile(false)}>Text</button>
                         </div>
                         {upload_type_is_file ? 
                             <FileUpload /> : 
-                            <textarea name="text"></textarea>
+                            <textarea className="upload-input" name="text"></textarea>
                         }
                     </li>
                 </ul>
                 <div className="form-footer">
+                    {error && !error.field ? <ErrorMessage error={error} /> : null}
                     <button type="submit">Post to GopherNotes</button>
                 </div>
             </form>
@@ -49,13 +62,21 @@ export default function Upload() {
 
 }
 
+function ErrorMessage(props: { error: UploadError }) {
+    return (
+        <div className="error-message">
+            <p>{props.error.msg}</p>
+        </div>
+    );
+}
+
 function FileUpload() {
 
     const [file_name, setFileName] = useState<null | string>(null);
     const file_input = useRef<HTMLInputElement>(null);
 
     return (
-        <button type="button" className="file-upload" onClick={() => file_input.current!.click()}>
+        <button type="button" className="upload-input file-upload" onClick={() => file_input.current!.click()}>
             <input ref={file_input} type="file" name="file" accept=".jpg,.jpeg,.png,.pdf" required 
                 onChange={() => setFileName(file_input.current?.files?.[0].name ?? null)} 
             />
@@ -71,9 +92,11 @@ function FileUpload() {
 
 }
 
-function upload(e: FormEvent<HTMLFormElement>, navigate: NavigateFunction) {
+function upload(e: FormEvent<HTMLFormElement>, navigate: NavigateFunction, setError: React.Dispatch<React.SetStateAction<UploadError | null>>) {
 
     e.preventDefault();
+
+    // setError(null);
 
     const form: HTMLFormElement = e.currentTarget;
 
@@ -90,7 +113,10 @@ function upload(e: FormEvent<HTMLFormElement>, navigate: NavigateFunction) {
             return;
         }
             
-        res.json().then((error: any) => console.log(error));
+        res.json().then((error: any) => {
+            // console.log(error);
+            setError(error);
+        });
         
     });
 
